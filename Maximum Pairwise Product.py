f87ed032e04a66347ca09e2d73447e638ddb34db
yeexue <38835254+yeexue@users.noreply.github.com>
2019-05-21 21:04:14 +0800

    Create Maximum Pairwise Product.py

---
+++ Maximum Pairwise Product.py
@@ -0,0 +1,25 @@
+# Uses python3
+len1 = input()
+n = input()
+a = [int(x) for x in n.strip().split()]
+le = int(len1)
+
+# check the largest number's index 
+
+index1 = 0
+
+for i1 in range(1,le):
+    if a[i1] > a[index1] :
+        index1 = i1
+
+# check the second largest number's index 
+
+if index1 == 0 : index2 = 1
+else: index2 = 0
+
+for i2 in range(1,le):
+    if a[i2] > a[index2] and i2 != index1 :
+        index2 = i2
+
+result = a[index2] * a[index1]
+print(result)
